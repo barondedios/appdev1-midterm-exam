3ce8bdcb424d0684e16d0f9584684ed14b16151b
barondedios <realbarondedios@gmail.com>
2025-10-04 11:19:02 +0800

    fixed the header in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,69 +8,58 @@ function App() {
 
   return (
     <>
-       {/* ***** Preloader Start ***** */}
-  {/* <div id="js-preloader" className="js-preloader">
-    <div className="preloader-inner">
-      <span className="dot" />
-      <div className="dots">
-        <span />
-        <span />
-        <span />
-      </div>
-    </div>
-  </div> */}
-  {/* ***** Preloader End ***** */}
-  {/* ***** Header Area Start ***** */}
-  <header
-    className="header-area header-sticky wow slideInDown"
-    data-wow-duration="0.75s"
-    data-wow-delay="0s"
-  >
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <nav className="main-nav">
-            {/* ***** Logo Start ***** */}
-            <a href="index.html" className="logo">
-              <img src="assets/images/logo.png" alt="Chain App Dev" />
-            </a>
-            {/* ***** Logo End ***** */}
-            {/* ***** Menu Start ***** */}
-            <ul className="nav">
-              <li className="scroll-to-section">
-                <a href="#top" className="active">
-                  Home
+
+<header
+        className="header-area header-sticky wow slideInDown"
+        data-wow-duration="0.75s"
+        data-wow-delay="0s"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <nav className="main-nav">
+                {/* ***** Logo Start ***** */}
+                <a href="index.html" className="logo">
+                  <img src="assets/images/logo.png" alt="Chain App Dev" />
                 </a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#services">Services</a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#about">About</a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#pricing">Pricing</a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#newsletter">Newsletter</a>
-              </li>
-              <li>
-                <div className="gradient-button">
-                  <a id="modal_trigger" href="#modal">
-                    <i className="fa fa-sign-in-alt" /> Sign In Now
-                  </a>
-                </div>
-              </li>
-            </ul>
-            <a className="menu-trigger">
-              <span>Menu</span>
-            </a>
-            {/* ***** Menu End ***** */}
-          </nav>
+                {/* ***** Logo End ***** */}
+                {/* ***** Menu Start ***** */}
+                <ul className="nav">
+                  <li className="scroll-to-section">
+                    <a href="#top" className="active">
+                      Home
+                    </a>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#services">Services</a>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#about">About</a>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#pricing">Pricing</a>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#newsletter">Newsletter</a>
+                  </li>
+                  <li>
+                    <div className="gradient-button">
+                      <a id="modal_trigger" href="#modal">
+                        <i className="fa fa-sign-in-alt" /> Sign In Now
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+                <a className="menu-trigger">
+                  <span>Menu</span>
+                </a>
+                {/* ***** Menu End ***** */}
+              </nav>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </header>
+      </header>
+
   {/* ***** Header Area End ***** */}
   <div id="modal" className="popupContainer" style={{ display: "none" }}>
     <div className="popupHeader">
