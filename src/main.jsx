57338f7956e9eb11ec04bae830eb19bc943f1144
barondedios <realbarondedios@gmail.com>
2025-10-04 10:16:44 +0800

    linked the assets in index.html, removed some lines in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './styles/animated.css';
-import './styles/fontawesome.css';
-import './styles/owl.css';
-import './styles/templatemo-chain-app-dev.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
