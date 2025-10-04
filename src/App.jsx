7e6c20924993122be0e442799d79fdd70c3e58b7
barondedios <realbarondedios@gmail.com>
2025-10-04 11:23:18 +0800

    fixed the header area end, social login, username & password form in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,76 +61,76 @@ function App() {
       </header>
 
   {/* ***** Header Area End ***** */}
-  <div id="modal" className="popupContainer" style={{ display: "none" }}>
-    <div className="popupHeader">
-      <span className="header_title">Login</span>
-      <span className="modal_close">
-        <i className="fa fa-times" />
-      </span>
-    </div>
-    <section className="popupBody">
-      {/* Social Login */}
-      <div className="social_login">
-        <div className="">
-          <a href="#" className="social_box fb">
-            <span className="icon">
-              <i className="fab fa-facebook" />
-            </span>
-            <span className="icon_title">Connect with Facebook</span>
-          </a>
-          <a href="#" className="social_box google">
-            <span className="icon">
-              <i className="fab fa-google-plus" />
-            </span>
-            <span className="icon_title">Connect with Google</span>
-          </a>
+ <div id="modal" className="popupContainer" style={{ display: "none" }}>
+        <div className="popupHeader">
+          <span className="header_title">Login</span>
+          <span className="modal_close">
+            <i className="fa fa-times" />
+          </span>
         </div>
-        <div className="centeredText">
-          <span>Or use your Email address</span>
-        </div>
-        <div className="action_btns">
-          <div className="one_half">
-            <a href="#" id="login_form" className="btn">
-              Login
-            </a>
+        <section className="popupBody">
+          {/* Social Login */}
+          <div className="social_login">
+            <div className="">
+              <a href="#" className="social_box fb">
+                <span className="icon">
+                  <i className="fab fa-facebook" />
+                </span>
+                <span className="icon_title">Connect with Facebook</span>
+              </a>
+              <a href="#" className="social_box google">
+                <span className="icon">
+                  <i className="fab fa-google-plus" />
+                </span>
+                <span className="icon_title">Connect with Google</span>
+              </a>
+            </div>
+            <div className="centeredText">
+              <span>Or use your Email address</span>
+            </div>
+            <div className="action_btns">
+              <div className="one_half">
+                <a href="#" id="login_form" className="btn">
+                  Login
+                </a>
+              </div>
+              <div className="one_half last">
+                <a href="#" id="register_form" className="btn">
+                  Sign up
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="one_half last">
-            <a href="#" id="register_form" className="btn">
-              Sign up
-            </a>
-          </div>
-        </div>
-      </div>
       {/* Username & Password Login form */}
-      <div className="user_login">
-        <form>
-          <label>Email / Username</label>
-          <input type="text" />
-          <br />
-          <label>Password</label>
-          <input type="password" />
-          <br />
-          <div className="checkbox">
-            <input id="remember" type="checkbox" />
-            <label htmlFor="remember">Remember me on this computer</label>
+     <div className="user_login">
+            <form>
+              <label>Email / Username</label>
+              <input type="text" />
+              <br />
+              <label>Password</label>
+              <input type="password" />
+              <br />
+              <div className="checkbox">
+                <input id="remember" type="checkbox" />
+                <label htmlFor="remember">Remember me on this computer</label>
+              </div>
+              <div className="action_btns">
+                <div className="one_half">
+                  <a href="#" className="btn back_btn">
+                    <i className="fa fa-angle-double-left" /> Back
+                  </a>
+                </div>
+                <div className="one_half last">
+                  <a href="#" className="btn btn_red">
+                    Login
+                  </a>
+                </div>
+              </div>
+            </form>
+            <a href="#" className="forgot_password">
+              Forgot password?
+            </a>
           </div>
-          <div className="action_btns">
-            <div className="one_half">
-              <a href="#" className="btn back_btn">
-                <i className="fa fa-angle-double-left" /> Back
-              </a>
-            </div>
-            <div className="one_half last">
-              <a href="#" className="btn btn_red">
-                Login
-              </a>
-            </div>
-          </div>
-        </form>
-        <a href="#" className="forgot_password">
-          Forgot password?
-        </a>
-      </div>
       {/* Register Form */}
       <div className="user_register">
         <form>
